0fd367e997c62d0030f15d815cb034ffffc77337
apatel924 <apatel2@ualberta.ca>
2023-06-19 20:48:09 -0600

    initial commit

---
+++ src/components/CheckoutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CheckoutPage = () => {
+    return (
+        <div>
+            <h1> CheckoutPage </h1>
+        </div>
+    );
+};
+
+export default CheckoutPage;
+
